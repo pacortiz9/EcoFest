f9e06a408ea80e1ef70b05820bcdf2dfd5f90f44
Paco <a01706532@tec.mx>
2025-06-09 13:23:01 -0600

    publicación del sitio web en dominio

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/ecofest', // 
+  assetPrefix: '/ecofest', //
 };
 
 export default nextConfig;
